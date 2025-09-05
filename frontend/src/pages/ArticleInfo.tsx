e627c3ead5fc3ef81d2b04355009b0726e4d9798
scamassa <sara.camassa@heig-vd.ch>
2025-09-05 21:54:23 +0200

    remove console log

--- frontend/src/pages/ArticleInfo.tsx
+++ frontend/src/pages/ArticleInfo.tsx
@@ -144,8 +144,6 @@ const ArticleInfo = () => {
       end_date: borrowDates[1]
     }
 
-    console.log(form);
-
     fetch(`${process.env.REACT_APP_BACKEND_URL}/borrows`, {
       method: 'POST',
       headers: {
